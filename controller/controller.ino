433c793dece5f5ee4f992399523a46e5185e548f
cTn <ctndesigner@gmail.com>
2013-03-01 23:03:15 +0100

    up 3

--- controller/controller.ino
+++ controller/controller.ino
@@ -327,9 +327,9 @@ void process100HzTask() {
     
     // Calculate Heading
     yawError = commandYawAttitude - kinematicsAngle[ZAXIS];
+    
     if (yawError > PI) yawError -= TWO_PI;
     else if (yawError < -PI) yawError += TWO_PI;         
-    Serial.println(yawError);
     
     if (flightMode == ATTITUDE_MODE) {
         // Compute command PIDs (with kinematics correction)
